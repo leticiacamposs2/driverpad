18d97cf7e92b9a2a57f28c82d00e9558841f0aed
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-07-19 02:01:59 -0300

    configuracao de testes usando jest e enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('deve renderizar corretamente em modo "debug"', () => {
+    const component = shallow(<App debug />);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('deve renderizar corretamente sem nenhum props', () => {
+    const component = shallow(<App />);
+
+    expect(component).toMatchSnapshot();
+  });
 });
